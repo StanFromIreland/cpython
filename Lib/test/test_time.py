a5ce1692d6765cb5f90265d39fe0a97c6b485a6c
Stan Ulbrych <stan@ulbrych.org>
2026-02-09 17:18:37 +0000

    Fix conflict "fix"

--- Lib/test/test_time.py
+++ Lib/test/test_time.py
@@ -358,13 +358,8 @@ class TimeTestCase(unittest.TestCase):
         # Should be able to go round-trip from strftime to strptime without
         # raising an exception.
         tt = time.gmtime(self.t)
-<<<<<<< e-d-no-year
-        for directive in ('a', 'A', 'b', 'B', 'c', 'd', 'e', 'H', 'I',
-                          'j', 'm', 'M', 'p', 'S',
-=======
-        for directive in ('a', 'A', 'b', 'B', 'c', 'd', 'F', 'H', 'I',
+        for directive in ('a', 'A', 'b', 'B', 'c', 'd', 'e', 'F', 'H', 'I',
                           'j', 'm', 'M', 'p', 'S', 'T',
->>>>>>> main
                           'U', 'w', 'W', 'x', 'X', 'y', 'Y', 'Z', '%'):
             format = '%' + directive
             if directive in ('d', 'e'):
